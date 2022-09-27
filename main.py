c4c7aea37acbcf05a3f858c986d00e912c297956
Wagner Anthony <59626222+zadhart@users.noreply.github.com>
2022-09-27 16:00:38 -0300

    Fixed some bugs on the WebDriver

--- main.py
+++ main.py
@@ -4,14 +4,28 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
+import json
+import uuid
+
+f = open("config.json")
+
+jdata = json.load(f)
+
+f.close()
+
+name = str(uuid.uuid4())
+
+jdata["files"].append(name)
+
+with open("config.json", "w") as outfile:
+    outfile.write(json.dumps(jdata))
 
 data = []
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.qconcursos.com/questoes-de-concursos/questoes?discipline_ids%5B%5D=4&subject_ids%5B%5D=15446")
 
-
-for i in range(30):
+for i in range(jdata["lastPage"], jdata["lastPage"] + 20):
     time.sleep(2)
 
     t = driver.find_elements(By.CLASS_NAME, "q-id")
@@ -26,8 +40,15 @@ for i in range(30):
 
     time.sleep(2)
 
-nd = pd.DataFrame(data, columns=['link'])
+    nd = pd.DataFrame(data, columns=['link'])
 
-nd.to_csv("questoes.csv")
+    nd.to_csv(str(name)+".csv")
 
-    
+    print(i)
+
+    jdata["lastPage"] = i
+
+    with open("config.json", "w") as outfile:
+        outfile.write(json.dumps(jdata))
+
+
